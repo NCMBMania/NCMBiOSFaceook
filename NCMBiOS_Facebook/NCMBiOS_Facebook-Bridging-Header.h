bb1f5f875175487ffe2d8045e4ead672e3ef29dd
Naoki Tsutsui <naokitsutsui@me.com>
2015-07-07 16:39:58 +0900

    プロジェクトを追加しました

---
+++ NCMBiOS_Facebook/NCMBiOS_Facebook-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <NCMB/NCMB.h>
+#import <NCMB/NCMBObject+Subclass.h>
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKShareKit/FBSDKShareKit.h>
---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '8.3'
+
+inhibit_all_warnings!
+
+# 2015/07/05時点では、このオプションは使用できません。（NCMB関連でエラーが発生します）
+# use_frameworks!
+
+pod 'NCMB', :git => 'https://github.com/NIFTYCloud-mbaas/ncmb_ios.git'
+
+pod "FBSDKCoreKit"
+pod "FBSDKLoginKit"
+pod "FBSDKShareKit"
